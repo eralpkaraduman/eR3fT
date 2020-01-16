a35a84a1de1dfe37fcac37ce11bf97f86cbfa6bb
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-01-17 01:01:26 +0200

    Auto generated experiment links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,38 @@
 import React from "react"
-// import { Link } from "gatsby"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Intro from "../components/intro"
 import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Box</h1>
-    <div
-      style={{
-        width: `300px`,
-        height: `300px`,
-        border: `1px solid black`,
-        borderRadius: `5px`,
-        marginBottom: `1.45rem`,
-      }}
-    >
-      <Intro />
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  const experiments = useStaticQuery(graphql`
+    query ExperimentsQuery {
+      allSitePage(filter: { path: { regex: "/experiments/" } }) {
+        edges {
+          node {
+            path
+            id
+            internalComponentName
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h2>Experiments</h2>
+      <ul>
+        {experiments.allSitePage.edges.map(({ node }) => (
+          <li key={node.id}>
+            <Link to={node.path}>
+              {node.internalComponentName.replace("ComponentExperiments", "")}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
