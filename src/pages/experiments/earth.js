d9497b768bb04e25ac7bc37e9f8ad4ca914e79ac
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-01-17 22:30:00 +0200

    Moved texture to static folder

---
+++ src/pages/experiments/earth.js
@@ -0,0 +1,31 @@
+import React, { Suspense } from "react"
+import Experiment from "../../components/experiment"
+import * as THREE from "three"
+import { Canvas, useFrame, useLoader } from "react-three-fiber"
+
+export default ({ children, width, height }) => {
+  return (
+    <Experiment width={200} height={200} title="Earth">
+      <Canvas pixelRatio={3}>
+        <Suspense fallback={null}>
+          <Earth />
+        </Suspense>
+      </Canvas>
+    </Experiment>
+  )
+}
+
+const Earth = () => {
+  const [texture] = useLoader(THREE.TextureLoader, ["/textures/earth.jpg"])
+  const ref = React.useRef()
+  useFrame(() => {
+    const { current: mesh } = ref
+    mesh.rotation.y += 0.02
+  })
+  return (
+    <mesh ref={ref} rotation={[0.9, 3, 0]}>
+      <sphereBufferGeometry attach="geometry" args={[2.8, 32, 32]} />
+      <meshBasicMaterial attach="material" color={0xffffff} map={texture} />
+    </mesh>
+  )
+}
