d01b7172661dae6e7714a2eb575ce9ea70fb0664
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-01-17 23:17:58 +0200

    Cleanup

--- src/pages/experiments/earth.js
+++ src/pages/experiments/earth.js
@@ -3,7 +3,7 @@ import Experiment from "../../components/experiment"
 import * as THREE from "three"
 import { Canvas, useFrame, useLoader } from "react-three-fiber"
 
-export default ({ children, width, height }) => {
+export default () => {
   return (
     <Experiment width={200} height={200} title="Earth">
       <Canvas pixelRatio={3}>
@@ -23,8 +23,8 @@ const Earth = () => {
     mesh.rotation.y += 0.02
   })
   return (
-    <mesh ref={ref} rotation={[0.9, 3, 0]}>
-      <sphereBufferGeometry attach="geometry" args={[2.8, 32, 32]} />
+    <mesh ref={ref} rotation={[0.5, 3, 0]}>
+      <sphereBufferGeometry attach="geometry" args={[2.6, 32, 32]} />
       <meshBasicMaterial attach="material" color={0xffffff} map={texture} />
     </mesh>
   )
