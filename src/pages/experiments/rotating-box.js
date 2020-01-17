5bafbecb770a2b471aaee45a0446ccb8f9e1c50e
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-01-17 22:32:12 +0200

    Minor tweaks

--- src/pages/experiments/rotating-box.js
+++ src/pages/experiments/rotating-box.js
@@ -3,16 +3,19 @@ import Experiment from "../../components/experiment"
 import * as THREE from "three"
 import { Canvas, useFrame } from "react-three-fiber"
 
+const boxSize = 50
+const boxPositionZ = -100
+
 export default ({ children, width, height }) => {
   return (
     <Experiment width={200} height={200} title="Rotating Box">
-      <Canvas camera={{ position: [0, 0, 0], fov: 40 }}>
+      <Canvas>
         <rectAreaLight
-          intensity={5}
+          intensity={2}
+          width={100}
+          height={100}
           position={[0, 0, 0]}
-          width={30}
-          height={30}
-          onUpdate={self => self.lookAt(new THREE.Vector3(0, 0, 0))}
+          onUpdate={self => self.lookAt(new THREE.Vector3(0, 0, boxPositionZ))}
         />
         <Box />
       </Canvas>
@@ -24,11 +27,11 @@ const Box = () => {
   const ref = React.useRef()
   useFrame(() => {
     const { current: mesh } = ref
-    mesh.rotation.x = mesh.rotation.y = mesh.rotation.z += 0.01
+    mesh.rotation.x = mesh.rotation.y += 0.01
   })
   return (
-    <mesh ref={ref} position={[0, 0, -60]}>
-      <boxBufferGeometry attach="geometry" args={[20, 20, 20]} />
+    <mesh ref={ref} position={[0, 0, boxPositionZ]}>
+      <boxBufferGeometry attach="geometry" args={[boxSize, boxSize, boxSize]} />
       <meshStandardMaterial color={0xffffff} attach="material" />
     </mesh>
   )
